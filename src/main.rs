6454e2833ed4076e755940341627c46c9227e497
M-Farag <mina.farag@icloud.com>
2023-06-02 14:36:51 +0300

    UPD Reading HTTP Requests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::net;
+use std::net::{TcpListener as tcp, TcpStream};
+use std::io::{prelude::*,BufReader};
+
 fn main() {
-    let listener = net::TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() 
-    {
+    let listener = tcp::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Stream: {:#?}",stream);
-        println!("Hello");
+        process_request(stream);
+        
     }
+}
+
+fn process_request(mut stream: TcpStream)
+{
+    let buff_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buff_reader.lines()
+    .map(|result|{result.unwrap()})
+    .take_while(|line|{! line.is_empty()})
+    .collect();
+
+    println!("HTTP Request: {:#?}",http_request);
 }
