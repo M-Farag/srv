85700499c156a69315822eedbdb0ca5f63e4bb3a
M-Farag <mina.farag@icloud.com>
2023-06-05 01:31:15 +0300

    ADD HTML Index page as the default response
    UPD Srvr main file to return 200 on any request with the HTML page

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener as tcp, TcpStream};
 use std::io::{prelude::*,BufReader};
+use std::fs;
 
 fn main() {
     let listener = tcp::bind("127.0.0.1:7878").unwrap();
@@ -18,9 +19,13 @@ fn process_request(mut stream: TcpStream)
     .take_while(|line|{! line.is_empty()})
     .collect();
 
-    let response = "HTTP/1.1 200 ok \r\n Content-Type: application/json \r\n\r\n";
-    let json_response = r#"{"Mina":"is Awesome"}"#;
-    let response = format!("{} {}",response,json_response.escape_default());
-    println!("{}",response);
+    // let response = "HTTP/1.1 200 ok \r\n Content-Type: application/json \r\n\r\n";
+    // let json_response = r#"{"Mina":"is Awesome"}"#;
+    // let response = format!("{} {}",response,json_response.escape_default());
+    let contents = fs::read_to_string("index.html").unwrap();
+    let contents_len = contents.len();
+    let status_line = "HTTP/1.1 200 loading index";
+    let headers = format!("Content-Length: {}",contents_len);
+    let response = format!("{}\r\n{}\r\n\r\n{}",status_line,headers,contents);
     stream.write_all(response.as_bytes()).unwrap()
 }
