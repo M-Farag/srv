f2dc88a64a9605ec38accca5798683b71d18008d
M-Farag <mina.farag@icloud.com>
2023-06-08 11:01:53 +0300

    UPD Simulate slow respose blocking other requests

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,29 @@ fn process_request(mut stream: TcpStream)
     // let response = "HTTP/1.1 200 ok \r\n Content-Type: application/json \r\n\r\n";
     // let json_response = r#"{"Mina":"is Awesome"}"#;
     // let response = format!("{} {}",response,json_response.escape_default());
-    let file_name;
-    let status_line;
-    if http_request[0].contains("GET / HTTP/1.1")
+    // let file_name: &str;
+    // let status_line: &str;
+    // if http_request[0].contains("GET / HTTP/1.1")
+    // {
+    //    file_name = "index.html";
+    //    status_line = "HTTP/1.1 200 loading index";
+    // }
+    // else
+    // {
+    //     file_name = "404.html";
+    //     status_line = "HTTP/1.1 404 Page not found";
+    // }
+    
+    let (status_line,file_name) = match &http_request[0][..]
     {
-       file_name = "index.html";
-       status_line = "HTTP/1.1 200 loading index";
-    }
-    else
-    {
-        file_name = "404.html";
-        status_line = "HTTP/1.1 404 Page not found";
-    }
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 loading index","index.html"),
+        "GET /sleep HTTP/1.1" => {
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            ("HTTP/1.1 200 loading sleep","index.html")
+        },
+        _ => ("HTTP/1.1 404 Page not found","404.html")
+    };
+
     let contents = fs::read_to_string(file_name).unwrap();
     let contents_len = contents.len();
     
